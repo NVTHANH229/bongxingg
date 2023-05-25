a82706497f12e307f082cd071fe9cc304d18e65a
NguyenQuynhh <flora.nguyen.vn@gmail.com>
2023-05-25 14:38:52 +0700

    box

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'home#index'
+  root to: 'book_borroweds#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
